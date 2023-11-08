2d34ac932107efc6d044e00a6cc648ad073320ef
Anish Nandyala <anandyal@terpmail.umd.edu>
2023-11-07 19:51:31 -0500

    one article

--- scripts.js
+++ scripts.js
@@ -26,19 +26,55 @@ window.addEventListener('DOMContentLoaded', () => {
 const commentContainer = document.getElementById('all-comments');
 document.getElementById('comment').addEventListener('keypress', function (ev) {
     if (ev.key == 'Enter') {
+        if (document.getElementById('comment').value.match(/\S/)) {
+            addComment(ev);
+        }
+    }
+});
+
+document.getElementById('name').addEventListener('keypress', function (ev) {
+    if (ev.key == 'Enter') {
         addComment(ev);
     }
 });
 
 function addComment (ev) {
-    let commentText, cardDiv, cardBodyDiv;
-    const textBox = document.createElement('div');
+    let commentText, nameText, cardDiv, cardBodyDiv, dFlexOuter, dFlexInner, likeDiv, likeButton, likeCounter;
+    const commentBox = document.createElement('div');
+    const nameBox = document.createElement('div');
     cardDiv = document.createElement('div');
+    cardBodyDiv = document.createElement('div');
+    dFlexOuter = document.createElement('div');
+    dFlexInner = document.createElement('div');
+    likeDiv = document.createElement('div');
+    likeButton = document.createElement('i');
+    likeCounter = document.createElement('div');
     cardDiv.className = 'card mb-4';
+    cardBodyDiv.className = 'card-body';
+    dFlexOuter.className = 'd-flex justify-content-between';
+    dFlexInner.className = 'd-flex flex-row align-items-center';
+    likeDiv.className = 'd-flex flex-row align-items-center';
+    likeButton.className = 'far fa-thumbs-up mx-2 fa-xs text-white';
+    likeButton.setAttribute('id','like-button');
+    likeCounter.className = 'small text-muted mb-0';
+    likeCounter.setAttribute('id','like-counter');
+    likeCounter.innerHTML = "0";
+    nameBox.className = 'small mb-0 ms-2';
+    nameText = document.getElementById('name').value;
+    document.getElementById('name').value = '';
     commentText = document.getElementById('comment').value;
     document.getElementById('comment').value = '';
-    textBox.style.paddingLeft = 10;
-    textBox.innerHTML = commentText;
-    cardDiv.append(textBox);
+    nameBox.innerHTML = nameText;
+    commentBox.innerHTML = commentText;
+    cardBodyDiv.style.marginTop = "30px";
+    dFlexInner.style.marginTop = "50px";
+    likeCounter.style.marginTop = "30px";
+    likeDiv.style.marginTop = "20px";
+    likeDiv.append(likeButton, likeCounter);
+    dFlexInner.append(nameBox);
+    dFlexOuter.append(dFlexInner, likeDiv);
+    cardBodyDiv.append(commentBox, dFlexOuter);
+    cardDiv.append(cardBodyDiv);
     commentContainer.appendChild(cardDiv);
 }
+
